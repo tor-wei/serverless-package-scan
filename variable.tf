e2497c257a3db77f5a99e2d3d47584c22fea16cf
tor-wei <gohtorwei@gmail.com>
2025-07-24 22:12:00 +0800

    Fix variable error

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable lambda_function_name {
     description     = "Name of lambda function"
     type            = string
-    default         = "${local.prefix}-package-scan-lambda-fn"
+    default         = "torwei-package-scan-lambda-fn"
 }
 
 variable lambda_file_name {
